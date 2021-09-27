47cd2ceb0bef40e15faf9e587ecd726c82e506a4
AntonZarodiuk <anton.zarodyuk@gmail.com>
2021-09-27 21:07:54 +0300

    commit with config files

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const config = require('./config/config.json');
 const mongoose = require('mongoose');
 const bot = require('./modules/bot-module');
+const express = require('express');
+const app = express();
 
 mongoose.connect(config.DBUrl);
 mongoose.connection.on('connected', () => {
     console.log('conection established');
     bot.launch();
 })
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => console.log(`[SERVER] listening ob port ${PORT}`))
